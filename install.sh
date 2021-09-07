e57072c05fe68722375aecfd23892b8abda4cf53
Nick Vladimiroff <nvladimiroff@overdrive.com>
2021-09-07 15:24:19 -0400

    Add an install script.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ~/.config/nvim
+ln -s "$(pwd)/init.vim" "~/.config/nvim/init.vim"
+ln -s "$(pwd)/.tmux.conf" "~/.tmux.conf"
+ln -s "$(pwd)/.tmux-wsl" "~/.tmux-wsl"
+ln -s "$(pwd)/.tmux-macos" "~/.tmux-macos"
