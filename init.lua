2d10798010b78835c5308c8de45284d79ef49ac8
Nick Vladimiroff <nvladimiroff@overdrive.com>
2024-12-13 16:23:47 -0500

    Update lots of stuff.

--- init.lua
+++ init.lua
@@ -56,7 +56,7 @@ require("lazy").setup({
 
 -- Telescope settings
 -- Don't enable these when embedded in VS Code though.
-if vim.fn.has 'gui_running' == 0
+if vim.fn.has 'gui_running' == 0 then
   local builtin = require('telescope.builtin')
   vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
   vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/alacritty
 ln -s "$(pwd)/init.lua" ~/.config/nvim/init.lua
+ln -s "$(pwd)/.gitconfig" ~/.gitconfig
 ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
 ln -s "$(pwd)/.alacritty.toml" ~/.config/alacritty/alacritty.toml
