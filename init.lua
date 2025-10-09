5cc0eaa736912f1c21e923cfb8d4b5a298dbfd8f
Nick Vladimiroff <nvladimiroff@overdrive.com>
2025-10-09 11:16:26 -0400

    Vim: rm blink, add lsp.

--- init.lua
+++ init.lua
@@ -61,62 +61,6 @@ require("lazy").setup({
     -- Easy comments!
     { 'numToStr/Comment.nvim', opts = {} },
 
-    -- Fancy autocomplete.
-    -- TODO: on the chopping block.
-    {
-      'saghen/blink.cmp',
-      -- optional: provides snippets for the snippet source
-      dependencies = { 'rafamadriz/friendly-snippets' },
-
-      -- use a release tag to download pre-built binaries
-      version = '1.*',
-      -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-      -- build = 'cargo build --release',
-      -- If you use nix, you can build from source using latest nightly rust with:
-      -- build = 'nix run .#build-plugin',
-
-      ---@module 'blink.cmp'
-      ---@type blink.cmp.Config
-      opts = {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-        -- 'super-tab' for mappings similar to vscode (tab to accept)
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- All presets have the following mappings:
-        -- C-space: Open menu or open docs if already open
-        -- C-n/C-p or Up/Down: Select next/previous item
-        -- C-e: Hide menu
-        -- C-k: Toggle signature help (if signature.enabled = true)
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = 'super-tab' },
-
-        appearance = {
-          -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-          -- Adjusts spacing to ensure icons are aligned
-          nerd_font_variant = 'mono'
-        },
-
-        -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = false } },
-
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
-        sources = {
-          default = { 'lsp', 'path', 'snippets', 'buffer' },
-        },
-
-        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-        --
-        -- See the fuzzy documentation for more information
-        fuzzy = { implementation = "prefer_rust_with_warning" }
-      },
-      opts_extend = { "sources.default" }
-    },
-
     -- File picker.
     -- TODO: on the chopping block.
     {
@@ -140,6 +84,9 @@ require("lazy").setup({
     -- More text objects.
     { 'nvim-treesitter/nvim-treesitter-textobjects' },
     { 'nvim-mini/mini.ai', version = '*', opts = {} },
+
+    -- LSP
+    { 'neovim/nvim-lspconfig' },
   },
   checker = { enabled = true },
 })
@@ -170,7 +117,7 @@ vim.cmd.colorscheme "catppuccin"
 --
 
 -- Neotree toggle.
-vim.keymap.set('n', '<leader>e', ':Neotree toggle=true<CR>')
+vim.keymap.set('n', '<leader>e', ':Neotree toggle=true<CR><Esc>')
 
 -- Double escape to clear searches.
 vim.keymap.set('n', '<Esc><Esc>', '<Esc>:nohlsearch<CR><Esc>', {silent = true})
@@ -200,3 +147,8 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertLeave" }, {
   end,
 })
 
+
+--
+-- LSP
+--
+vim.lsp.enable('ruby_lsp')
