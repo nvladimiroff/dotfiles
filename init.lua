e63b3c6ab797ad3683d11c55a285d44b7bc647ef
Nick Vladimiroff <nvladimiroff@overdrive.com>
2025-10-10 10:11:18 -0400

    Vim: keybinds for moving around buffers.

--- init.lua
+++ init.lua
@@ -137,6 +137,9 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+vim.keymap.set('n', 'J', '<Esc>:bn<CR>', {silent = true})
+vim.keymap.set('n', 'H', '<Esc>:bp<CR>', {silent = true})
+
 
 --
 -- AUTOSAVE
