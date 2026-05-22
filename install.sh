d959d4733548839c2191a9c7db01cc1905146666
Nick Vladimiroff <nick@codeandcake.dev>
2026-05-21 20:59:38 -0400

    Add JJ.

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/jj
 ln -s "$(pwd)/init.lua" ~/.config/nvim/init.lua
 ln -s "$(pwd)/.gitconfig" ~/.gitconfig
 ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
 ln -s "$(pwd)/.alacritty.toml" ~/.config/alacritty/alacritty.toml
+ln -s "$(pwd)/jj.toml" ~/.config/jj/config.toml
